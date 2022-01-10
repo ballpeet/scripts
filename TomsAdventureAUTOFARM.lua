2bc7222a973d1a2475aa6bb51f187cb35a037465
ballpeet <94201310+ballpeet@users.noreply.github.com>
2022-01-10 02:51:24 -0500

    Update TomsAdventureAUTOFARM.lua

--- TomsAdventureAUTOFARM.lua
+++ TomsAdventureAUTOFARM.lua
@@ -58,8 +58,7 @@ videoframe.Position = UDim2.new(0,0,0,0)
 videoframe.Looped = true
 videoframe.Volume = 0
 videoframe.BorderSizePixel = 0
-videoframe.Video = getsynasset("kyands.webm")
-warn(getsynasset("kyands.webm"))
+videoframe.Video = getsynasset("workspace/kyands.webm")
 
 local espswitch = false
 local autocollectswitch = false
