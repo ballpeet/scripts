2ec147c26dff4b65d94ba0f01f1def6bb61ae5e5
ballpeet <94201310+ballpeet@users.noreply.github.com>
2023-12-30 19:22:57 -0500

    Update pjjautofarm.lua

--- pjjautofarm.lua
+++ pjjautofarm.lua
@@ -2,28 +2,22 @@ local killLoop = coroutine.wrap(function()
 	repeat
 		task.wait(0.5)
 		local success, errormes = pcall(function()
-			character = player.Character
-			ownVoiceline = character.Torso:FindFirstChild("voiceline")
-			humroot = character.HumanoidRootPart
-
-            if buttonValues[dummySmiterToggle] == true then
-				for i2, v2 in pairs(game.Workspace:GetChildren()) do
-					if v2.Name ~= "Rubber Dummy" then
-						local humthing = v2:FindFirstChild("Humanoid")
-						local plrfound = game.Players:GetPlayerFromCharacter(v2)
-						if humthing and plrfound == nil then
-							local torso = v2:FindFirstChild("HumanoidRootPart") or v2:FindFirstChild("Torso")
-							spawn(function ()
-								if buttonValues[spawnDinosToggle] == true then
-									local got = hitbox:InvokeServer(65, torso, torso.CFrame, 0.5, ownVoiceline, humthing, false, false, false)
-								end
-								if humthing.Health > 0 then
-									local got = hitbox:InvokeServer(55, torso, torso.CFrame, 20000, ownVoiceline, humthing, false, false, false)
-								end
-							end)
-						end
-					end
-				end
+            for i2, v2 in pairs(game.Workspace:GetChildren()) do
+                if v2.Name ~= "Rubber Dummy" then
+                    local humthing = v2:FindFirstChild("Humanoid")
+                    local plrfound = game.Players:GetPlayerFromCharacter(v2)
+                    if humthing and plrfound == nil then
+                        local torso = v2:FindFirstChild("HumanoidRootPart") or v2:FindFirstChild("Torso")
+                        spawn(function ()
+                            if buttonValues[spawnDinosToggle] == true then
+                                local got = hitbox:InvokeServer(65, torso, torso.CFrame, 0.5, torso, humthing, false, false, false)
+                            end
+                            if humthing.Health > 0 then
+                                local got = hitbox:InvokeServer(55, torso, torso.CFrame, 20000, torso, humthing, false, false, false)
+                            end
+                        end)
+                    end
+                end
             end
 		end)
 		if not success then
